701e8088c6063375a1075c966f87a64a7c104a2a
Heiko van der Heijden <h.j.m.van.der.heijden@hva.nl>
2019-05-09 11:39:00 +0200

    Additional work on Python

--- PythonWebServer/PythonServer.py
+++ PythonWebServer/PythonServer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # Example webserver based written in Python
+# Author Heiko van der Heijden
 import socket
 
 # Some Global Variables
@@ -13,12 +14,17 @@ HTML_RESPONSE = "<!DOCTYPE html><html><body><h1>Hello world!</h1></body></html>"
 HTTP_RESP = "HTTP/1.1 200 OK\r\nServer: PythonWebServer\r\nPragma: no-cache\r\nExpires: 0 \r\nContent-Type: text/html\r\nContent-Length: %i\r\n\r\n%s"
 
 print ("Starting Server")
+print("Please go with your browser to http://%s:%i" % (HOST, PORT))
+
+# Open a TCP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
-sock.listen(100)
+sock.listen(100) # timeout
 
 try:
+    print("Press control c to quit")
     while True:
+        # Accept a request
         conn, addr = sock.accept()
         print("Connected by ", addr)
         data = conn.recv(INPUT_BUFFER).decode("utf-8")
@@ -28,10 +34,10 @@ try:
         # Create a response based on the HTML and the http headers
         response = HTTP_RESP % (len(HTML_RESPONSE), HTML_RESPONSE)
         
-        # Encode it
+        # Encode it to bytes
         response = response.encode('utf-8')
-        conn.sendall(response)
-
+        conn.sendall(response) 
+        
         conn.close()
 except KeyboardInterrupt:
     print("Closing program")
